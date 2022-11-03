b2249f877ab5bb764e1e5249567ebd909bb96c8f
futokiyo <futokiyoc@terra.dti.ne.jp>
2022-11-03 17:56:56 +0900

    Initial Commit

---
+++ src/main/java/io/futokiyo/spring_boot3_web_mvc_demonov03/Demonov03Application.java
@@ -0,0 +1,13 @@
+package io.futokiyo.spring_boot3_web_mvc_demonov03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demonov03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demonov03Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/futokiyo/spring_boot3_web_mvc_demonov03/Demonov03ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.futokiyo.spring_boot3_web_mvc_demonov03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demonov03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
